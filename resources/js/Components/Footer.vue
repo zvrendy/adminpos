49155d94b18e1e65b77925ac1997fd939c51ac09
Endy Mooduto <endy.mooduto87@gmail.com>
2022-04-18 09:32:00 +0700

    update tes

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -2,7 +2,7 @@
 
   <footer class="main-footer">
     <strong
-      >Copyright &copy; 2022 <a href="https://adminlte.io">AdminPOS</a>.</strong
+      >Copyright &copy; 2022 <a href="https://zvrendy.github.io/">AdminPOS</a>.</strong
     >
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block"><b>Version</b> 3.2.0</div>
